1930a3ef5723a43aebf2ba81bc36c1dcf3a7b45c
hungify <nmhungify@gmail.com>
2023-01-06 13:51:26 +0700

    feat: use better password hash lib

--- src/modules/auth/password.service.ts
+++ src/modules/auth/password.service.ts
@@ -1,22 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { compare, hash } from 'bcrypt';
+import * as argon2 from 'argon2';
 import type { SecurityConfig } from '~/common/interfaces';
 
 @Injectable()
 export class PasswordService {
   constructor(private configService: ConfigService) {}
 
-  private get bcryptSaltRounds() {
+  private get saltRounds() {
     const { saltRounds } = this.configService.get<SecurityConfig>('security');
     return saltRounds;
   }
 
-  compare(password: string, hashedPassword: string) {
-    return compare(password, hashedPassword);
+  verify(password: string, hashedPassword: string) {
+    return argon2.verify(hashedPassword, password);
   }
 
   hash(password: string) {
-    return hash(password, this.bcryptSaltRounds);
+    return argon2.hash(password, { salt: Buffer.alloc(this.saltRounds) });
   }
 }
