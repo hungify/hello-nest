f7505475139e6da9dcbbd3c0f1835332f6251687
hungify <nmhungify@gmail.com>
2023-12-17 20:20:50 +0700

    refactor: create email module

---
+++ src/modules/email/dtos/forgot-password.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class EmailForgotPasswordDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  subject: string;
+
+  @IsString()
+  @IsNotEmpty()
+  verifyUrl: string;
+}
---
+++ src/modules/email/email.service.ts
@@ -0,0 +1,65 @@
+import { IEmailService } from './email.interface';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import fs from 'fs';
+import mustache from 'mustache';
+import path from 'path';
+import { GmailConfig } from '~/common/types/config.type';
+import { EmailRegisterDto } from './dtos/register.dto';
+import { EmailHelper } from './email.helper';
+import { EmailForgotPasswordDto } from './dtos/forgot-password.dto';
+
+@Injectable()
+export class EmailService implements IEmailService {
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly emailHelper: EmailHelper,
+  ) {}
+
+  async register({ email, subject, verifyUrl }: EmailRegisterDto) {
+    try {
+      const templatePath = path.join(__dirname, 'templates', 'register.html');
+      const html = fs.readFileSync(templatePath, 'utf8');
+
+      const gmail = this.configService.get<GmailConfig>('gmail');
+
+      const info = {
+        from: `Application name 👻 ${gmail.user}`,
+        to: email,
+        subject: subject,
+        html: mustache.render(html, { link: verifyUrl }),
+      };
+
+      this.emailHelper.sendEmail(info);
+      return true;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new InternalServerErrorException(error.message);
+      }
+      return false;
+    }
+  }
+  async forgotPassword({ email, subject, verifyUrl }: EmailForgotPasswordDto) {
+    try {
+      const templatePath = path.join(__dirname, 'templates', 'register.html');
+      const html = fs.readFileSync(templatePath, 'utf8');
+
+      const gmail = this.configService.get<GmailConfig>('gmail');
+
+      const info = {
+        from: `Application name 👻 ${gmail.user}`,
+        to: email,
+        subject: subject,
+        html: mustache.render(html, { link: verifyUrl }),
+      };
+
+      this.emailHelper.sendEmail(info);
+      return true;
+    } catch (error) {
+      if (error instanceof Error) {
+        throw new InternalServerErrorException(error.message);
+      }
+      return false;
+    }
+  }
+}
