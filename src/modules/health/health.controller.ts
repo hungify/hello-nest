b19787f75b150d45f8e668cd490101890a1b55df
hungify <nmhungify@gmail.com>
2024-02-25 13:05:44 +0700

    Refactor health controller to use config service for API URL

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,22 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import {
   HealthCheckService,
   HttpHealthIndicator,
   HealthCheck,
 } from '@nestjs/terminus';
+import { AppConfig, HttpConfig } from '~/common/types/config.type';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
+    private configService: ConfigService<AppConfig>,
   ) {}
 
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([
-      () => this.http.pingCheck('nestjs-docs', 'https://docs.nestjs.com'),
-    ]);
+    const { host, port } = this.configService.get<HttpConfig>('http');
+    const url = `http://${host}:${port}/api`;
+    return this.health.check([() => this.http.pingCheck('api', url)]);
   }
 }
