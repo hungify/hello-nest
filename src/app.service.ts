9720b121b946f51b2894aa94c6b2e818134176fe
hungify <nmhungify@gmail.com>
2026-08-30 09:18:35 +0700

    WIP

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import type { Request } from 'express';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   welcome(req: Request): Record<string, string> {
+    const swaggerPath = this.configService.get<string>('swagger.path', {
+      infer: true,
+    });
+    const host =
+      this.configService.get<string>('http.host', { infer: true }) ||
+      req.get('Host');
+    const port = this.configService.get<number>('http.port', { infer: true });
+    const protocol = req.protocol;
+    const link =
+      port && port !== 80 && port !== 443
+        ? `${protocol}://${host}:${port}${swaggerPath}`
+        : `${protocol}://${host}${swaggerPath}`;
+
     return {
       title: 'Welcome to NestJS API Stater',
-      content: 'Redirect to /api/docs for API documentation',
-      link: `${req.protocol}://${req.get('Host')}${req.originalUrl}api/docs`,
+      content: `Redirect to ${swaggerPath} for API documentation`,
+      link,
     };
   }
 }
